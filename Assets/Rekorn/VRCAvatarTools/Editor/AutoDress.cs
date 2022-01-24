e4a8f4cb30b3ff78b21ac15481076f9ce842c2b4
urun4m0r1 <urun4m0r1@gmail.com>
2022-01-24 22:36:28 +0900

    프로젝트 구조 생성

---
+++ Assets/Rekorn/VRCAvatarTools/Editor/AutoDress.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AutoDress : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
