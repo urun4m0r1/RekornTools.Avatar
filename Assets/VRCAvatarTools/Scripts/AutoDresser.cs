a4a04b0170f9bbc10349d8410a4e74ad77711b85
urun4m0r1 <urun4m0r1@gmail.com>
2022-01-26 23:32:56 +0900

    오토드레스 기본 기능 구현

--- Assets/VRCAvatarTools/Scripts/AutoDresser.cs
+++ Assets/VRCAvatarTools/Scripts/AutoDresser.cs
@@ -1,10 +1,9 @@
 ﻿#if UNITY_EDITOR
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
+using NaughtyAttributes;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Rekorn.VRCAvatarTools
 {
@@ -17,23 +16,29 @@ namespace Rekorn.VRCAvatarTools
     [Serializable]
     public class RigNamingConvention
     {
+        [SerializeField] public RigModifierPosition ModifierPosition;
+        [SerializeField] public string Splitter = ".";
         [SerializeField] public string ModifierLeft = "Left";
         [SerializeField] public string ModifierRight = "Right";
-        [SerializeField] public string Splitter = ".";
-        [SerializeField] public RigModifierPosition Position;
 
-        public string Preview
-        {
-            get
-            {
-                if (Position == RigModifierPosition.Front)
-                    return $"{ModifierLeft}{Splitter}Arm" +
-                           $"{ModifierRight}{Splitter}Leg";
-                else
-                    return $"Arm{Splitter}{ModifierLeft}" +
-                           $"Leg{Splitter}{ModifierRight}";
-            }
-        }
+        public string PreviewText =>
+            ModifierPosition == RigModifierPosition.Front
+                ? $"{ModifierLeft}{Splitter}Arm / " +
+                  $"{ModifierRight}{Splitter}Leg"
+                : $"Arm{Splitter}{ModifierLeft} / " +
+                  $"Leg{Splitter}{ModifierRight}";
+    }
+
+    [Serializable]
+    public class RigNamingCombination
+    {
+        [SerializeField] public RigNamingConvention NamingConvention;
+        public string LeftFront => $"{NamingConvention.ModifierLeft}{NamingConvention.Splitter}";
+        public string RightFront => $"{NamingConvention.ModifierRight}{NamingConvention.Splitter}";
+        public string LeftEnd => $"{NamingConvention.Splitter}{NamingConvention.ModifierLeft}";
+        public string RightEnd => $"{NamingConvention.Splitter}{NamingConvention.ModifierRight}";
+
+        public RigNamingCombination(RigNamingConvention namingConvention) => NamingConvention = namingConvention;
     }
 
     [Serializable]
@@ -46,22 +51,33 @@ namespace Rekorn.VRCAvatarTools
     [Serializable]
     public struct RigNamePair
     {
-        [SerializeField] public bool IgnoreChildren;
-        [SerializeField] public string Avatar;
-        [SerializeField] public string Cloth;
+        [SerializeField] public bool DisableParenting;
+        [SerializeField] public string AvatarBoneName;
+        [SerializeField] public string ClothBoneName;
     }
 
 
     [ExecuteInEditMode]
     public sealed class AutoDresser : MonoBehaviour
     {
+        [Header("Rig Settings")]
         [SerializeField] private AvatarRig _avatar;
         [SerializeField] private AvatarRig _cloth;
-        [SerializeField] private string _prefix;
-        [SerializeField] private string _suffix;
+
+        [Header("Dress Settings")]
+        [SerializeField] private bool _unpackClothMeshes = true;
+        [SerializeField] private bool _deleteLeftover = true;
+        [SerializeField] private string _clothPrefix;
+        [SerializeField] private string _clothSuffix;
         [SerializeField] private List<RigNamePair> _rigNameExceptions;
 
-        public void Apply()
+        [Header("Debug Info")]
+        [ReadOnly] public bool DebugEnabled = true;
+        [ShowNativeProperty] private string AvatarNamingPreview => _avatar.NamingConvention.PreviewText;
+        [ShowNativeProperty] private string ClothNamingPreview => _cloth.NamingConvention.PreviewText;
+        [ShowNativeProperty] private string NewClothNamingPreview => $"{_clothPrefix}Hips{_clothSuffix}";
+
+        public void ChangeNamingConvention()
         {
             //find gameObject named "Armature" in _clothRoot and add all gameObject inside Armature prefix and suffix
             var armature = _cloth.Rig.transform.Find("Armature");
@@ -80,21 +96,156 @@ namespace Rekorn.VRCAvatarTools
             var children = armature.GetComponentsInChildren<Transform>();
             foreach (var child in children)
             {
-                var newChild = FindRecursive(_avatar.Rig.transform, child.name);
-                if (newChild == null)
+                var newName = child.name;
+                var clothModifierPosition = _cloth.NamingConvention.ModifierPosition;
+                var avatarModifierPosition = _avatar.NamingConvention.ModifierPosition;
+
+                var clothCombination = new RigNamingCombination(_cloth.NamingConvention);
+                var avatarCombination = new RigNamingCombination(_avatar.NamingConvention);
+
+                if (clothModifierPosition == RigModifierPosition.Front)
                 {
-                    Debug.LogError("Can't find " + child.name + " in " + _avatar.Rig.name);
-                    continue;
+                    if (newName.Contains(clothCombination.LeftFront))
+                    {
+                        newName = newName.Replace(clothCombination.LeftFront, "");
+
+                        if (avatarModifierPosition == RigModifierPosition.Front)
+                        {
+                            newName = $"{avatarCombination.LeftFront}{newName}";
+                        }
+                        else
+                        {
+                            newName = $"{newName}{avatarCombination.LeftEnd}";
+                        }
+                    }
+                    else if (newName.Contains(clothCombination.RightFront))
+                    {
+                        newName = newName.Replace(clothCombination.RightFront, "");
+
+                        if (avatarModifierPosition == RigModifierPosition.Front)
+                        {
+                            newName = $"{avatarCombination.RightFront}{newName}";
+                        }
+                        else
+                        {
+                            newName = $"{newName}{avatarCombination.RightEnd}";
+                        }
+                    }
+                }
+                else
+                {
+                    if (newName.Contains(clothCombination.LeftEnd))
+                    {
+                        newName = newName.Replace(clothCombination.LeftEnd, "");
+
+                        if (avatarModifierPosition == RigModifierPosition.Front)
+                        {
+                            newName = $"{avatarCombination.LeftFront}{newName}";
+                        }
+                        else
+                        {
+                            newName = $"{newName}{avatarCombination.LeftEnd}";
+                        }
+                    }
+                    else if (newName.Contains(clothCombination.RightEnd))
+                    {
+                        newName = newName.Replace(clothCombination.RightEnd, "");
+
+                        if (avatarModifierPosition == RigModifierPosition.Front)
+                        {
+                            newName = $"{avatarCombination.RightFront}{newName}";
+                        }
+                        else
+                        {
+                            newName = $"{newName}{avatarCombination.RightEnd}";
+                        }
+                    }
                 }
 
-                var newName = _prefix + child.name + _suffix;
-                child.name = newName;
+                foreach (var exception in _rigNameExceptions)
+                {
+                    if (newName.Contains(exception.ClothBoneName))
+                    {
+                        newName = newName.Replace(exception.ClothBoneName, exception.AvatarBoneName);
+                    }
+                }
 
-                //set child parent to newChild's parent
-                child.SetParent(newChild.parent);
-                child.localPosition = newChild.localPosition;
-                child.localRotation = newChild.localRotation;
-                child.localScale = newChild.localScale;
+                child.name = newName;
+            }
+        }
+
+        public void ApplyCloth()
+        {
+            //find gameObject named "Armature" in _clothRoot and add all gameObject inside Armature prefix and suffix
+            var armature = _cloth.Rig.transform.Find("Armature");
+            if (armature == null)
+            {
+                Debug.LogError("Can't find Armature in " + _cloth.Rig.name);
+                return;
+            }
+
+            // if _clothRoot is prefab, unpack it
+            if (PrefabUtility.GetPrefabInstanceStatus(_cloth.Rig.gameObject) == PrefabInstanceStatus.Connected)
+            {
+                Debug.LogWarning("Unpack " + _cloth.Rig.name + " prefab first!");
+                return;
+            }
+
+            var children = armature.GetComponentsInChildren<Transform>();
+            foreach (var child in children)
+            {
+                var disableParenting = false;
+
+                foreach (var exception in _rigNameExceptions)
+                {
+                    if (child.name.Contains(exception.AvatarBoneName))
+                    {
+                        disableParenting = exception.DisableParenting;
+                    }
+                }
+
+                var newParent = FindRecursive(_avatar.Rig.transform, child.name);
+                if (newParent == null)
+                {
+                    newParent = child.parent;
+                }
+
+                Undo.RecordObject(child.gameObject, "Apply Cloth");
+                child.name = $"{_clothPrefix}{child.name}{_clothSuffix}";
+
+                if (disableParenting)
+                {
+                    Undo.SetTransformParent(child, newParent.parent, "Parenting");
+                }
+                else
+                {
+                    Undo.SetTransformParent(child, newParent, "Parenting");
+                }
+            }
+
+            Undo.SetTransformParent(_cloth.Rig.transform, _avatar.Rig.transform, "Parenting");
+
+            if (_unpackClothMeshes)
+            {
+                var clothMeshes = _cloth.Rig.GetComponentsInChildren<SkinnedMeshRenderer>();
+                foreach (var clothMesh in clothMeshes)
+                {
+                    Undo.SetTransformParent(clothMesh.gameObject.transform, _avatar.Rig.transform, "Parenting");
+                }
+            }
+
+            if (_deleteLeftover)
+            {
+                Undo.DestroyObjectImmediate(armature.gameObject);
+
+                if (_unpackClothMeshes)
+                {
+                    Undo.DestroyObjectImmediate(_cloth.Rig.gameObject);
+                }
+                else
+                {
+                    Undo.DestroyObjectImmediate(_cloth.Rig);
+                }
             }
         }
 
---
+++ Assets/VRCAvatarTools/Scripts/AutoDresserEditor.cs
@@ -0,0 +1,32 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace Rekorn.VRCAvatarTools
+{
+    [CustomEditor(typeof(AutoDresser))]
+    public sealed class AutoDresserEditor : Editor
+    {
+        private AutoDresser _target;
+
+        private void OnEnable()
+        {
+            _target = (AutoDresser)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (GUILayout.Button("Change Naming Convention"))
+            {
+                _target.ChangeNamingConvention();
+            }
+            if (GUILayout.Button("Apply Cloth"))
+            {
+                _target.ApplyCloth();
+            }
+        }
+    }
+}
+#endif
