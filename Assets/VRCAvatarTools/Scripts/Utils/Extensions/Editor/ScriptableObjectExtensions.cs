8d5358809b049458182133935412e42d09fee496
urun4m0r1 <urun4m0r1@gmail.com>
2022-04-25 22:41:53 +0900

    Null 안정성 추가

--- Assets/VRCAvatarTools/Scripts/Utils/Extensions/Editor/ScriptableObjectExtensions.cs
+++ Assets/VRCAvatarTools/Scripts/Utils/Extensions/Editor/ScriptableObjectExtensions.cs
@@ -16,7 +16,11 @@ namespace VRCAvatarTools
         [NotNull] public static T GetInstance<T>([CanBeNull] this T obj) where T : ScriptableObject =>
             (obj == null ? CreateInstance<T>() : obj) ?? throw new InvalidOperationException();
 
-        public static void DrawEditor([NotNull] this UnityEngine.Object obj) =>
-            UnityEditor.Editor.CreateEditor(obj)?.OnInspectorGUI();
+        public static void DrawEditor([NotNull] this UnityEngine.Object obj)
+        {
+            var editor = UnityEditor.Editor.CreateEditor(obj);
+
+            if (editor) editor.OnInspectorGUI();
+        }
     }
 }
