a8c64e0681c3c4f85b89b5f681e61938daab05d7
urun4m0r1 <urun4m0r1@gmail.com>
2022-05-08 19:23:07 +0900

    버전 체크 구현

--- Assets/RekornTools/Core/VersionChecker.cs
+++ Assets/RekornTools/Core/VersionChecker.cs
@@ -1,19 +1,65 @@
-﻿using UnityEditor;
+﻿using System;
+using System.Threading;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace RekornTools
 {
+    [Serializable]
+    public struct VersionInfo
+    {
+        [SerializeField] public string version;
+        [SerializeField] public string server;
+        [SerializeField] public string github;
+        [SerializeField] public string booth;
+    }
+
     [InitializeOnLoad]
     public class VersionChecker
     {
-        static VersionChecker()
-        {
-            CheckNewVersion();
-        }
+        static VersionChecker() => new Thread(CheckNewVersion).Start();
 
         static void CheckNewVersion()
         {
-            Debug.Log("Checking for new version...");
+            var textAsset = AssetDatabase.LoadAssetAtPath<TextAsset>("Assets/RekornTools/Core/VERSION.json");
+            if (textAsset == null)
+            {
+                Debug.LogError("VersionChecker: Could not find server file.");
+                return;
+            }
+
+            var localVersion = JsonUtility.FromJson<VersionInfo>(textAsset.text);
+
+            var www = UnityWebRequest.Get(localVersion.server);
+
+            var request = www?.SendWebRequest();
+            if (request == null)
+            {
+                Debug.LogError("VersionChecker: Can't reach server.");
+                return;
+            }
+
+            request.completed += x =>
+            {
+                if (www.isNetworkError || www.isHttpError)
+                {
+                    Debug.Log($"VersionChecker: {www.error}");
+                }
+                else
+                {
+                    var remoteVersion = JsonUtility.FromJson<VersionInfo>(www.downloadHandler?.text);
+                    if (remoteVersion.version != localVersion.version)
+                    {
+                        EditorUtility.DisplayDialog("New version available"
+                                                  , $"New version {remoteVersion.version} is available.\n"
+                                                  + $"You can download it from here.\n"
+                                                  + $"Github: {remoteVersion.github}\n"
+                                                  + $"Booth: {remoteVersion.booth}"
+                                                  , "OK");
+                    }
+                }
+            };
         }
     }
 }
