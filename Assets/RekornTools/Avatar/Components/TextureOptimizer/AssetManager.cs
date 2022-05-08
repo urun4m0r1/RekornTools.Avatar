5c9dc27037251945dad9155a6155bfc1e862a8de
urun4m0r1 <urun4m0r1@gmail.com>
2022-05-08 18:36:37 +0900

    AssetManager 완성

--- Assets/RekornTools/Avatar/Components/TextureOptimizer/AssetManager.cs
+++ Assets/RekornTools/Avatar/Components/TextureOptimizer/AssetManager.cs
@@ -9,47 +9,39 @@ namespace RekornTools.Avatar
     [ExecuteInEditMode]
     public sealed class AssetManager : MonoBehaviour
     {
-        [Header("Target")]
-        [SerializeField] Transform _parent;
+        [SerializeField] public Transform Parent;
 
-        [Header("Renderers")]
-        [SerializeField] [ReadOnlyList] [NotNull] Renderers _renderers = new Renderers();
-
-        [Header("Materials")]
-        [SerializeField] [ReadOnlyList] [NotNull] Materials _materials = new Materials();
-
-        [Header("Shaders")]
-        [SerializeField] [ReadOnlyList] [NotNull] Shaders _shaders = new Shaders();
-
-        [Header("Textures")]
-        [SerializeField] [ReadOnlyList] [NotNull] Textures _textures = new Textures();
+        [SerializeField] [ReadOnlyList] [NotNull] public Renderers Renderers = new Renderers();
+        [SerializeField] [ReadOnlyList] [NotNull] public Materials Materials = new Materials();
+        [SerializeField] [ReadOnlyList] [NotNull] public Shaders   Shaders   = new Shaders();
+        [SerializeField] [ReadOnlyList] [NotNull] public Textures  Textures  = new Textures();
 
         [SerializeField] [HideInInspector] Transform _prevParent;
 
-        void Awake() => _renderers.Initialize(_parent);
+        void Awake() => Renderers.Initialize(Parent);
 
         void OnValidate()
         {
-            if (_prevParent != _parent) Refresh();
+            if (_prevParent != Parent) Refresh();
         }
 
         [Button]
         void Refresh()
         {
-            _prevParent = _parent;
-            _renderers.Initialize(_parent);
+            _prevParent = Parent;
+            Renderers.Initialize(Parent);
 
-            _materials.Clear();
-            _shaders.Clear();
-            _textures.Clear();
+            Materials.Clear();
+            Shaders.Clear();
+            Textures.Clear();
 
             foreach (var (material, shader) in
-                     from r in _renderers
+                     from r in Renderers
                      from material in r.sharedMaterials
                      select (material, material.shader))
             {
-                if (!_materials.Contains(material)) _materials.Add(material);
-                if (!_shaders.Contains(shader)) _shaders.Add(shader);
+                if (!Materials.Contains(material)) Materials.Add(material);
+                if (!Shaders.Contains(shader)) Shaders.Add(shader);
 
                 AppendTexturesList(material, shader);
             }
@@ -62,7 +54,7 @@ namespace RekornTools.Avatar
             {
                 if (ShaderUtil.GetPropertyType(shader, i) != ShaderUtil.ShaderPropertyType.TexEnv) continue;
                 var texture = material.GetTexture(ShaderUtil.GetPropertyName(shader, i));
-                if (texture != null && !_textures.Contains(texture)) _textures.Add(texture);
+                if (texture != null && !Textures.Contains(texture)) Textures.Add(texture);
             }
         }
     }
---
+++ Assets/RekornTools/Avatar/Components/TextureOptimizer/Editor/AssetManagerEditor.cs
@@ -0,0 +1,38 @@
+﻿using JetBrains.Annotations;
+using UnityEditor;
+using UnityEngine;
+using static RekornTools.Avatar.Editor.EditorGUILayoutExtensions;
+using static UnityEditor.EditorGUILayout;
+
+namespace RekornTools.Avatar.Editor
+{
+    [CustomEditor(typeof(AssetManager))]
+    public sealed class AssetManagerEditor : BaseEditor<AssetManager>
+    {
+        bool _renderersFoldout;
+        bool _materialsFoldout;
+        bool _shadersFoldout;
+        bool _texturesFoldout;
+
+        protected override void Draw(AssetManager t)
+        {
+            t.Parent = ObjectField("Parent", t.Parent, true);
+
+            DrawLists(ref _renderersFoldout, nameof(AssetManager.Renderers), t.Renderers);
+            DrawLists(ref _materialsFoldout, nameof(AssetManager.Materials), t.Materials);
+            DrawLists(ref _shadersFoldout,   nameof(AssetManager.Shaders),   t.Shaders);
+            DrawLists(ref _texturesFoldout,  nameof(AssetManager.Textures),  t.Textures);
+        }
+
+        void DrawLists<T>(ref bool foldout, [NotNull] string property, [NotNull] ObjectList<T> list) where T : Object
+        {
+            BeginHorizontal();
+            {
+                foldout = Foldout(foldout, typeof(T).Name);
+                if (GUILayout.Button("Select All")) list.SelectComponents();
+            }
+            EndHorizontal();
+            if (foldout) PropertyField(serializedObject.ResolveProperty(property), true);
+        }
+    }
+}
