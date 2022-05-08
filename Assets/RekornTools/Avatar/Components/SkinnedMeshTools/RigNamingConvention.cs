ffc966cdd0c12be7567a657db32ec80defa8a11d
urun4m0r1 <urun4m0r1@gmail.com>
2022-05-08 21:52:14 +0900

    Naming 기본값 추가

--- Assets/RekornTools/Avatar/Components/SkinnedMeshTools/RigNamingConvention.cs
+++ Assets/RekornTools/Avatar/Components/SkinnedMeshTools/RigNamingConvention.cs
@@ -12,12 +12,12 @@ namespace RekornTools.Avatar
     }
 
     [Serializable]
-    public struct RigNamingConvention
+    public class RigNamingConvention
     {
-        [field: SerializeField] public ModifierType ModifierType  { get; private set; }
-        [field: SerializeField] public string       Splitter      { get; private set; }
-        [field: SerializeField] public string       ModifierLeft  { get; private set; }
-        [field: SerializeField] public string       ModifierRight { get; private set; }
+        [field: SerializeField] public ModifierType ModifierType  { get; private set; } = ModifierType.End;
+        [field: SerializeField] public string       Splitter      { get; private set; } = ".";
+        [field: SerializeField] public string       ModifierLeft  { get; private set; } = "L";
+        [field: SerializeField] public string       ModifierRight { get; private set; } = "R";
 
         [NotNull] public string LeftFront  => $"{ModifierLeft}{Splitter}";
         [NotNull] public string RightFront => $"{ModifierRight}{Splitter}";
