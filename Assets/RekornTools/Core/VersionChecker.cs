835b6c44c8e88c4a287e22235e2ce5a4d1a7e54a
urun4m0r1 <urun4m0r1@gmail.com>
2022-05-08 18:53:45 +0900

    버전 체커 추가

---
+++ Assets/RekornTools/Core/VersionChecker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace RekornTools
+{
+    [InitializeOnLoad]
+    public class VersionChecker
+    {
+        static VersionChecker()
+        {
+            CheckNewVersion();
+        }
+
+        static void CheckNewVersion()
+        {
+            Debug.Log("Checking for new version...");
+        }
+    }
+}
